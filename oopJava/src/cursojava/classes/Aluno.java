3b9ee309178eb968482e864b474fdcf0906207ce
thfmjdrip <thiagodrip@gmail.com>
2024-06-14 16:30:51 -0300

    Inicio de oop

---
+++ oopJava/src/cursojava/classes/Aluno.java
@@ -0,0 +1,14 @@
+package cursojava.classes;
+
+public class Aluno {
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	
+
+}
---
+++ oopJava/src/cursojava/exec/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.exec;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
